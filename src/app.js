37d2e51123099b9ec48ffd77c5f99ca6555c0907
Vishal <vishalofficial224@gmail.com>
2026-07-15 10:53:47 +0530

    made my code clean

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ init().then(() => {
 bot.on("message", async (msg) => {
   const songs = await search(msg.text);
   tempSongData.set(msg.chat.id, songs);
+  // console.log(tempSongData);
   const chatId = msg.chat.id;
   const buttons = Buttons(songs);
 
@@ -44,6 +45,22 @@ bot.on("callback_query", async (query) => {
     const songs = tempSongData.get(query.message.chat.id);
     const filePath = await downloadAudio(query.data);
     const song = songs.find((s) => s.videoId === query.data);
+    console.log(song)
+    const waitingMessage = await bot.sendMessage(
+      query.message.chat.id,
+      "⏳ Please wait a moment. Some songs take a little longer to download.",
+    );
+
+    setTimeout(async () => {
+      try {
+        await bot.deleteMessage(
+          query.message.chat.id,
+          waitingMessage.message_id,
+        );
+      } catch (err) {
+        console.error("Couldn't delete message:", err);
+      }
+    }, 3000);
     await bot.sendAudio(query.message.chat.id, fs.createReadStream(filePath), {
       title: song.name,
     });
@@ -52,7 +69,7 @@ bot.on("callback_query", async (query) => {
     console.log(error);
     await bot.sendMessage(
       query.message.chat.id,
-      "X Download failed. \n\nPlease try another result from the list or search for a different song.",
+      "❌ Download failed.\n\nYouTube is currently limiting download requests.\n\nPlease try again in a few moments, choose another song from the list, or search for a different song.",
     );
   }
 });
